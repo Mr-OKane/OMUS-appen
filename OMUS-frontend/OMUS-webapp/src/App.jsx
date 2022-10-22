d59a2fff4fe5c1fbd7f825f60fac3c620ee03d08
Mr-OKane <104971960+Mr-OKane@users.noreply.github.com>
2022-10-22 12:26:01 +0200

    Adjusted navbar

--- OMUS-frontend/OMUS-webapp/src/App.jsx
+++ OMUS-frontend/OMUS-webapp/src/App.jsx
@@ -13,21 +13,11 @@ const App = () =>  (
 
         <div className= {`bg-primary ${styles.flexStart}`} >
           <div className = {`${styles.boxWidth}`}>
-            <Hero />
           </div>
         </div>
 
         <div className= {`bg-primary ${styles.paddingX} ${styles.flexStart}`} >
           <div className = {`${styles.boxWidth}`}>
-            <Stats />
-            <Business />
-            <Billing />
-            <CardDeal />
-            <Testimonials />
-            <CardDeal />
-            <Clients />
-            <CTA />
-            <Footer />
           </div>
         </div>
       </div>
