363c6daf4e0485647f19b34dbea0bdab591c1bc1
Mr-OKane <emilokane14@gmail.com>
2022-10-24 16:45:03 +0200

    Fixed node problem

--- OMUS-frontend/OMUS-webapp/src/App.jsx
+++ OMUS-frontend/OMUS-webapp/src/App.jsx
@@ -1,27 +1,34 @@
-import styles from './style';
+import styles from "./style";
 
-import {Navbar, Hero, Stats, Business, Billing, CardDeal, Clients, CTA, Footer, Testimonials} from "./components";
+import {
+  Navbar,
+  Hero,
+  Stats,
+  Business,
+  Billing,
+  CardDeal,
+  Clients,
+  CTA,
+  Footer,
+  Testimonials,
+} from "./components";
 
-
-const App = () =>  (
-    <div className ="bg-primary w-full overflow-hidden">
-      <div className= {`${styles.paddingX} ${styles.flexCenter}`}>
-          <div className ={`${styles.boxWidth}`}>
-            <Navbar />
-          </div> 
-        </div>
-
-        <div className= {`bg-primary ${styles.flexStart}`} >
-          <div className = {`${styles.boxWidth}`}>
-          </div>
-        </div>
-
-        <div className= {`bg-primary ${styles.paddingX} ${styles.flexStart}`} >
-          <div className = {`${styles.boxWidth}`}>
-          </div>
-        </div>
+const App = () => (
+  <div className="bg-primary w-full overflow-hidden">
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Navbar />
       </div>
-  );
+    </div>
 
-export default App
+    <div className={`bg-primary ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}></div>
+    </div>
 
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}></div>
+    </div>
+  </div>
+);
+
+export default App;
