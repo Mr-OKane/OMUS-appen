41b5c4a9fa1aafe05d425b7d46e80e48d92665c9
William Anker <William.s.anker@gmail.com>
2023-08-08 15:07:05 +0200

    Added all the necessary files for ZipCode v1.21

---
+++ back-end API/OMUS_api/app/Http/Controllers/ZipCodeController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ZipCode;
+use App\Http\Requests\StoreZipCodeRequest;
+use App\Http\Requests\UpdateZipCodeRequest;
+
+class ZipCodeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreZipCodeRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ZipCode $zipCode)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateZipCodeRequest $request, ZipCode $zipCode)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ZipCode $zipCode)
+    {
+        //
+    }
+}
