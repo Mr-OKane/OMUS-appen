bf2787fc0631c620fb21981b66077620d1982340
William Anker <William.s.anker@gmail.com>
2023-08-08 14:50:31 +0200

    Added all the necessary files for Idea v1.7

---
+++ back-end API/OMUS_api/app/Http/Controllers/IdeaController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Idea;
+use App\Http\Requests\StoreIdeaRequest;
+use App\Http\Requests\UpdateIdeaRequest;
+
+class IdeaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreIdeaRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Idea $idea)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateIdeaRequest $request, Idea $idea)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Idea $idea)
+    {
+        //
+    }
+}
