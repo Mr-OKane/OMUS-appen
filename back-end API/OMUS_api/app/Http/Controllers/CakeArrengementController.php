40bd375ce2eb895a817051cc442f08b967fd5b48
William Anker <William.s.anker@gmail.com>
2023-08-08 14:44:55 +0200

    Added all the necessary files for CakeArrengement v1.3

---
+++ back-end API/OMUS_api/app/Http/Controllers/CakeArrengementController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\CakeArrengement;
+use App\Http\Requests\StoreCakeArrengementRequest;
+use App\Http\Requests\UpdateCakeArrengementRequest;
+
+class CakeArrengementController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreCakeArrengementRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(CakeArrengement $cakeArrengement)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateCakeArrengementRequest $request, CakeArrengement $cakeArrengement)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(CakeArrengement $cakeArrengement)
+    {
+        //
+    }
+}
