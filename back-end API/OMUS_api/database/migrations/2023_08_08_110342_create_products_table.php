566c3c983b1b9267ef4ab3ad3b7b83bf89c6abb6
William Anker <William.s.anker@gmail.com>
2023-08-10 11:12:36 +0200

    completed the migration file for the products table v1.16.1

--- back-end API/OMUS_api/database/migrations/2023_08_08_110342_create_products_table.php
+++ back-end API/OMUS_api/database/migrations/2023_08_08_110342_create_products_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('description')->nullable();
+            $table->longText('image');
+            $table->double('price');
             $table->timestamps();
         });
     }
