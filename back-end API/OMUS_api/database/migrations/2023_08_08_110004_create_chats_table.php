8b4da886e1194d141ec9e7283dc15b8e1d3a129d
William Anker <William.s.anker@gmail.com>
2023-08-10 12:16:00 +0200

    completed the migration file for the chats table v1.4.1

--- back-end API/OMUS_api/database/migrations/2023_08_08_110004_create_chats_table.php
+++ back-end API/OMUS_api/database/migrations/2023_08_08_110004_create_chats_table.php
@@ -13,7 +13,10 @@ return new class extends Migration
     {
         Schema::create('chats', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('message_id')->constrained('messages')->nullOnDelete();
+            $table->string('name');
             $table->timestamps();
+            $table->timestamp('deleted_at')->nullable();
         });
     }
 
