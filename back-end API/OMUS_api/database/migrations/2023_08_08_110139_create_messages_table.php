fe66263cd77d6bf60f07b1accb23f9d43fdda916
William Anker <William.s.anker@gmail.com>
2023-08-10 12:14:34 +0200

    completed the migration file for the messages table v1.9.1

--- back-end API/OMUS_api/database/migrations/2023_08_08_110139_create_messages_table.php
+++ back-end API/OMUS_api/database/migrations/2023_08_08_110139_create_messages_table.php
@@ -13,7 +13,10 @@ return new class extends Migration
     {
         Schema::create('messages', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->string('message');
             $table->timestamps();
+            $table->timestamp('deleted_at');
         });
     }
 
