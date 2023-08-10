fac86617c3152bac12efac7c80087ea08bffa6a8
William Anker <William.s.anker@gmail.com>
2023-08-10 12:14:49 +0200

    completed the migration file for the instruments table v1.8.1

--- back-end API/OMUS_api/database/migrations/2023_08_08_110125_create_instruments_table.php
+++ back-end API/OMUS_api/database/migrations/2023_08_08_110125_create_instruments_table.php
@@ -13,7 +13,9 @@ return new class extends Migration
     {
         Schema::create('instruments', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
             $table->timestamps();
+            $table->timestamp('deleted_at')->nullable();
         });
     }
 
