226ab60fa5d9ea6788da5b62b8a14565c5255f35
William Anker <William.s.anker@gmail.com>
2023-08-10 12:15:13 +0200

    completed the migration file for the ideas table v1.7.1

--- back-end API/OMUS_api/database/migrations/2023_08_08_110045_create_ideas_table.php
+++ back-end API/OMUS_api/database/migrations/2023_08_08_110045_create_ideas_table.php
@@ -13,7 +13,9 @@ return new class extends Migration
     {
         Schema::create('ideas', function (Blueprint $table) {
             $table->id();
+            $table->string('idea');
             $table->timestamps();
+            $table->timestamp('deleted_at')->nullable();
         });
     }
 
