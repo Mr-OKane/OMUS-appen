93b54d5c905161e5aa746b1bc3fcd5fe4ee62c7e
William Anker <William.s.anker@gmail.com>
2023-08-10 12:18:03 +0200

    completed the migration file for the absences table v1.1.1

--- back-end API/OMUS_api/database/migrations/2023_08_08_105615_create_absences_table.php
+++ back-end API/OMUS_api/database/migrations/2023_08_08_105615_create_absences_table.php
@@ -13,7 +13,11 @@ return new class extends Migration
     {
         Schema::create('absences', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->boolean('absence');
+            $table->date('absence_date');
             $table->timestamps();
+            $table->timestamp('deleted_at')->nullable();
         });
     }
 
