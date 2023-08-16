8bdf8519547cc1760e81238209c5b341d65b9391
William Anker <William.s.anker@gmail.com>
2023-08-16 11:28:04 +0200

    completed the Chat Factory v1.3.2

--- back-end API/OMUS_api/database/factories/ChatFactory.php
+++ back-end API/OMUS_api/database/factories/ChatFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Message;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,7 +18,8 @@ class ChatFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'name' => $this->faker->word(),
+            'message_id' => Message::all()->random(1)[0],['id'],
         ];
     }
 }
