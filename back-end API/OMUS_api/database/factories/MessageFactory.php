272f514fabf07c3feccd61c16664d751806199c3
William Anker <William.s.anker@gmail.com>
2023-08-16 14:50:40 +0200

    completed the message Factory v1.9.2

--- back-end API/OMUS_api/database/factories/MessageFactory.php
+++ back-end API/OMUS_api/database/factories/MessageFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,7 +18,8 @@ class MessageFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'message' => $this->faker->sentence(),
+            'user_id' => User::all()->random(1)[0]['id'],
         ];
     }
 }
