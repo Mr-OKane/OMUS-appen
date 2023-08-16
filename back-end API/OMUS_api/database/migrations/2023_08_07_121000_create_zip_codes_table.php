261542f9a1dff39687c3e45a973866a9040a2328
William Anker <William.s.anker@gmail.com>
2023-08-16 11:18:13 +0200

    moved the zip_codes migration v1.21.1.1

--- back-end API/OMUS_api/database/migrations/2023_08_07_121000_create_zip_codes_table.php
+++ back-end API/OMUS_api/database/migrations/2023_08_07_121000_create_zip_codes_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('zip_codes', function (Blueprint $table) {
             $table->id();
             $table->foreignId('city_id')->constrained('cities')->onDelete('cascade');
+            $table->string('zip_code')->unique();
             $table->timestamps();
             $table->timestamp('deleted_at')->nullable();
         });
