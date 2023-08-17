f1e4317abc40c8a29cda9f7b4d410d0cfa34cfe3
William Anker <William.s.anker@gmail.com>
2023-08-17 09:21:30 +0200

    completed the Database Seeder v1.22.3

--- back-end API/OMUS_api/database/seeders/DatabaseSeeder.php
+++ back-end API/OMUS_api/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Chat;
+use App\Models\ChatRoom;
+use App\Models\PracticeDate;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,6 +15,30 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        $this->call([
+            NotificationSeeder::class,
+            IdeaSeeder::class,
+            PermissionSeeder::class,
+            RoleSeeder::class,
+            InstrumentSeeder::class,
+            TeamSeeder::class,
+            StatusSeeder::class,
+            UserSeeder::class,
+            PracticeDateSeeder::class,
+            CakeArrengementSeeder::class,
+            SheetSeeder::class,
+            AbsenceSeeder::class,
+            MessageSeeder::class,
+            ChatSeeder::class,
+            ChatRoomSeeder::class,
+            CitySeeder::class,
+            ZipCodeSeeder::class,
+            AddressSeeder::class,
+            OrderStatusSeeder::class,
+            OrderSeeder::class,
+            ProductSeeder::class,
+            OrderProductsSeeder::class,
+        ]);
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
