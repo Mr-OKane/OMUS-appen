c7bbe8c69cb24ac424f5298ae79d455db2d43b2e
William Anker <William.s.anker@gmail.com>
2023-08-17 09:11:34 +0200

    completed the PracticeDate Seeder v1.14.3

--- back-end API/OMUS_api/database/seeders/PracticeDateSeeder.php
+++ back-end API/OMUS_api/database/seeders/PracticeDateSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\PracticeDate;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,6 @@ class PracticeDateSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        PracticeDate::factory()->count(50)->create();
     }
 }
