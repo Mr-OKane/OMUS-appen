058d1d2cc6f3263674012796caba16c75a47e443
William Anker <William.s.anker@gmail.com>
2023-08-17 09:16:34 +0200

    completed the City Seeder v1.6.3

--- back-end API/OMUS_api/database/seeders/CitySeeder.php
+++ back-end API/OMUS_api/database/seeders/CitySeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\City;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use League\Csv\Reader;
 
 class CitySeeder extends Seeder
 {
@@ -12,6 +14,18 @@ class CitySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $city = "";
+        $csv = Reader::createFromPath(storage_path('CSV\Postnummer2.csv'),'r+')
+            ->setHeaderOffset(0);
+
+        $records = $csv->getRecords();
+        foreach ($records as $offset => $record){
+            if($city != $record['Bynavn']){
+                City::firstOrCreate([
+                    'city' => $record['Bynavn'],
+                ]);
+                $city = $record['Bynavn'];
+            }
+        }
     }
 }
