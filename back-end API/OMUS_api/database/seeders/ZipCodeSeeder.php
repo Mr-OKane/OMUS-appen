b4d3e11689556abb24f58b7166e53290817bb459
William Anker <William.s.anker@gmail.com>
2023-08-17 09:06:53 +0200

    completed the ZipCode Seeder v1.21.3

--- back-end API/OMUS_api/database/seeders/ZipCodeSeeder.php
+++ back-end API/OMUS_api/database/seeders/ZipCodeSeeder.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\City;
+use App\Models\ZipCode;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use League\Csv\Reader;
 
 class ZipCodeSeeder extends Seeder
 {
@@ -12,6 +15,15 @@ class ZipCodeSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $csv = Reader::createFromPath(storage_path('CSV\Postnummer2.csv'),'r')
+            ->setHeaderOffset(0);
+        $records = $csv->getRecords();
+
+        foreach ($records as $offset => $record){
+            ZipCode::firstOrCreate([
+                'city_id' => City::where('city','=',$record['Bynavn'])->first()->id,
+                'zip_code' => $record['Postnr'],
+            ]);
+        }
     }
 }
