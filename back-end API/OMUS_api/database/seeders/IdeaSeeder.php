02e2557a9d36a392ca21457a5708d859fdd141f4
William Anker <William.s.anker@gmail.com>
2023-08-17 09:15:32 +0200

    completed the Idea Seeder v1.7.3

--- back-end API/OMUS_api/database/seeders/IdeaSeeder.php
+++ back-end API/OMUS_api/database/seeders/IdeaSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Idea;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,6 @@ class IdeaSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Idea::factory()->count(50)->create();
     }
 }
