932987b7d7a2051e2b3f7aa0935e1d55d19f549d
William Anker <William.s.anker@gmail.com>
2023-08-17 09:15:13 +0200

    completed the Instrument Seeder v1.8.3

--- back-end API/OMUS_api/database/seeders/InstrumentSeeder.php
+++ back-end API/OMUS_api/database/seeders/InstrumentSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Instrument;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,6 @@ class InstrumentSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        //Instrument::factory()->count(20)->create();
     }
 }
