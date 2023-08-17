8dc3c1c1f90e0a349e14857018be260538180ae1
William Anker <William.s.anker@gmail.com>
2023-08-17 11:17:15 +0200

    completted the Address model v1.2.4

--- back-end API/OMUS_api/app/Models/Address.php
+++ back-end API/OMUS_api/app/Models/Address.php
@@ -4,8 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Address extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'address'
+    ];
+
+    public function zipCode():BelongsTo
+    {
+        return $this->belongsTo(ZipCode::class,'zip_code_id');
+    }
+
+    public function users():HasMany
+    {
+        return $this->hasMany(User::class,'address_id','id');
+    }
 }
