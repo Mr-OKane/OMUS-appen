70819a25cbc1d01205cbae11ddadc60b544fd0d9
William Anker <William.s.anker@gmail.com>
2023-08-21 11:02:06 +0200

    completed the Status model v1.18.4

--- back-end API/OMUS_api/app/Models/Status.php
+++ back-end API/OMUS_api/app/Models/Status.php
@@ -4,8 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Status extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'name'
+    ];
+
+    public function orders(): HasMany
+    {
+        return $this->hasMany(Order::class,'status_id','id');
+    }
+
+    public function users(): HasMany
+    {
+        return $this->hasMany(User::class,'status_id','id');
+    }
+
 }
