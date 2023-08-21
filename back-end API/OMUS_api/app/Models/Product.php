870021525ddec416e0be22dbb46ce9994ac81940
William Anker <William.s.anker@gmail.com>
2023-08-21 11:00:58 +0200

    completed the Product model v1.15.4

--- back-end API/OMUS_api/app/Models/Product.php
+++ back-end API/OMUS_api/app/Models/Product.php
@@ -4,8 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Product extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'image',
+        'price',
+    ];
+
+    public function orders(): BelongsToMany
+    {
+        return $this->belongsToMany(Order::class,'order_products','product_id','order_id');
+    }
 }
