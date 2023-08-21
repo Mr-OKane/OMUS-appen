f731190faf3c36842580931884e2b9242700c338
William Anker <William.s.anker@gmail.com>
2023-08-21 10:56:18 +0200

    completed the CakeArrengement model v1.3.4

--- back-end API/OMUS_api/app/Models/CakeArrengement.php
+++ back-end API/OMUS_api/app/Models/CakeArrengement.php
@@ -4,8 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class CakeArrengement extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class,'user_id');
+    }
+
+    public function practiceDate(): BelongsTo
+    {
+        return $this->belongsTo(PracticeDate::class,'practice_date_id');
+    }
 }
