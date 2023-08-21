1990c60efc8ff41b5c01d66791873dbac7bc4d9f
William Anker <William.s.anker@gmail.com>
2023-08-21 10:59:50 +0200

    completed the Order model v1.11.4

--- back-end API/OMUS_api/app/Models/Order.php
+++ back-end API/OMUS_api/app/Models/Order.php
@@ -4,8 +4,35 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Order extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'order_date'
+    ];
+
+    public function address(): BelongsTo
+    {
+        return $this->belongsTo(Address::class,'address_id');
+    }
+
+    public function products(): BelongsToMany
+    {
+        return $this->belongsToMany(Product::class,'order_products', 'order_id','product_id');
+    }
+
+    public function status(): BelongsTo
+    {
+        return $this->belongsTo(Status::class, 'status_id');
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class,'user_id');
+    }
 }
