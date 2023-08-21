c0669509f1bd58cc706e14e64956e365d9a208dd
William Anker <William.s.anker@gmail.com>
2023-08-21 10:58:15 +0200

    completed the City model v1.6.4

--- back-end API/OMUS_api/app/Models/City.php
+++ back-end API/OMUS_api/app/Models/City.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class City extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'city'
+    ];
+
+    public function zipCodes(): HasMany
+    {
+        return $this->hasMany(ZipCode::class,'city_id','id');
+    }
 }
