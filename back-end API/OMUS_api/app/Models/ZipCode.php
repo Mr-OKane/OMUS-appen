173ed4d37c091c261a4f6408d5756c970ca373f0
William Anker <William.s.anker@gmail.com>
2023-08-21 11:02:51 +0200

    completed the ZipCode model v1.21.4

--- back-end API/OMUS_api/app/Models/ZipCode.php
+++ back-end API/OMUS_api/app/Models/ZipCode.php
@@ -4,8 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class ZipCode extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'zip_code'
+    ];
+
+    public function addresses(): HasMany
+    {
+        return $this->hasMany(Address::class,'zip_code_id','id');
+    }
+
+    public function city(): BelongsTo
+    {
+        return $this->belongsTo(City::class,'city_id','id');
+    }
 }
