adf45308f3f52d94a9416defa52271f2273363a0
William Anker <William.s.anker@gmail.com>
2023-08-21 11:00:25 +0200

    completed the Permission model v1.13.4

--- back-end API/OMUS_api/app/Models/Permission.php
+++ back-end API/OMUS_api/app/Models/Permission.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Permission extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'name'
+    ];
+
+    public function roles(): BelongsToMany
+    {
+        return $this->belongsToMany(Role::class,'role_permissions','permission_id','role_id');
+    }
 }
