19486ac7b97c8074fcc058b56c1480709ceb5af7
William Anker <William.s.anker@gmail.com>
2023-08-21 10:59:10 +0200

    completed the Message model v1.9.4

--- back-end API/OMUS_api/app/Models/Message.php
+++ back-end API/OMUS_api/app/Models/Message.php
@@ -4,8 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Message extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'Message'
+    ];
+
+    public function user():BelongsTo
+    {
+        return $this->belongsTo(User::class,'user_id');
+    }
+
+    public function chat(): BelongsTo
+    {
+        return $this->belongsTo(Chat::class,'chat_id');
+    }
 }
