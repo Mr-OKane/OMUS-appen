4299219ec65e349563ea5b16fcb73e908adaeeb0
William Anker <William.s.anker@gmail.com>
2023-08-21 10:57:50 +0200

    completed the chat model v1.4.4

--- back-end API/OMUS_api/app/Models/Chat.php
+++ back-end API/OMUS_api/app/Models/Chat.php
@@ -4,8 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Chat extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'name'
+    ];
+
+    public function chatRoom(): BelongsTo
+    {
+       return $this->belongsTo(ChatRoom::class,'chat_room_id');
+    }
+
+    public function messages(): HasMany
+    {
+        return $this->hasMany(Message::class,'chat_id','id');
+    }
 }
