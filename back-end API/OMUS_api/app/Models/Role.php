19b35b5310265c20584e7d6c990fcea93bf65468
William Anker <William.s.anker@gmail.com>
2023-08-21 11:01:15 +0200

    completed the Role model v1.16.4

--- back-end API/OMUS_api/app/Models/Role.php
+++ back-end API/OMUS_api/app/Models/Role.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Role extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'name'
+    ];
+
+    public function permissions(): BelongsToMany
+    {
+        return $this->belongsToMany(Permission::class,'role_permissions','role_id','permission_id');
+    }
 }
