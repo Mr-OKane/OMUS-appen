f6f6e0f4e5136bcf6fce42f77de01074a62a8ccd
William Anker <William.s.anker@gmail.com>
2023-08-21 11:00:41 +0200

    completed the PracticeDate model v1.14.4

--- back-end API/OMUS_api/app/Models/PracticeDate.php
+++ back-end API/OMUS_api/app/Models/PracticeDate.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class PracticeDate extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'practice_date'
+    ];
+
+    public function users(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class,'user_practice_dates','practice_date_id','user_id');
+    }
 }
