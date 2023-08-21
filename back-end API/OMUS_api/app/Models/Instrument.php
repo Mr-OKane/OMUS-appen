5eb2e82d416888db68050f3c9bed946ebec952f7
William Anker <William.s.anker@gmail.com>
2023-08-21 10:58:51 +0200

    completed the Instrument model v1.8.4

--- back-end API/OMUS_api/app/Models/Instrument.php
+++ back-end API/OMUS_api/app/Models/Instrument.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Instrument extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'name'
+    ];
+
+    public function users(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class,'user_instruments','instrument_id','user_id')->withTimestamps();
+    }
 }
