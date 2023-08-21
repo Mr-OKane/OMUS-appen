506520ec599276826b7110dee0a4ca15f77670e7
William Anker <William.s.anker@gmail.com>
2023-08-21 11:00:05 +0200

    completed the OrderProduct model v1.12.4

--- back-end API/OMUS_api/app/Models/OrderProduct.php
+++ back-end API/OMUS_api/app/Models/OrderProduct.php
@@ -4,8 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class OrderProduct extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'total_price'
+    ];
+
+    public function orders(): HasMany
+    {
+        return $this->hasMany(Order::class,'order_id');
+    }
+
+    public function products(): HasMany
+    {
+        return $this->hasMany(Product::class,'product_id');
+    }
 }
