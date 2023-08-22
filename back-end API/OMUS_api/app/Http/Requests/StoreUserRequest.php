7b21735872165640e9b904c5b1bc43ebf6eca5e0
William Anker <William.s.anker@gmail.com>
2023-08-22 14:26:59 +0200

    completed the user store and update requests v1.20.5

--- back-end API/OMUS_api/app/Http/Requests/StoreUserRequest.php
+++ back-end API/OMUS_api/app/Http/Requests/StoreUserRequest.php
@@ -11,7 +11,7 @@ class StoreUserRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,37 @@ class StoreUserRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'firstname' => "required|string|max:255",
+            'lastname' => "required|string|max:255",
+            'email' => "required|email:rfc",
+            'password' => "required|string|min:8|max:50|regex:/^.*(?=.{3,})(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[\d\x])(?=.*[!¤£$#%&@{}$+?|<>*]).*$/|",
+            'phoneNumber' => "required|string|max:11",
+            'address' => "required|integer|digits_between:1,20",
+            'role' => "required|integer|digits_between:1,20",
+            'status' => "required|integer|digits_between:1,20",
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'firstname.required' => "Brugeren skal have et fornavn, med mellemnavne.",
+            'firstname.max' => "Brugerens fornavn og mellemnavne kan ikke være længere end 255 tegn.",
+            'lastname.required' => "Brugeren skal have en efternavn.",
+            'lastname.max' => "Brugens efternavn kan ikke være længere end 255 tegn.",
+            'email.required' => "Brugeren skal have en email.",
+            'email.max' => "Brugerens email kan ikke være længere end 255 tegn.",
+            'password.required' => "Brugeren skal have et password",
+            'password.min' => "Brugerens kodeord skal være minium 8 tegn.",
+            'password.regex' => "Brugerens kodeord skal have 1 stort 1 lille 1 tal og 1 special tegn.",
+            'phoneNumber.required' => "Brugerens skal have et telefon nummmer.",
+            'phoneNumber.max' => "Brugerens telefon nummer kan ikke være længere end 11 tegn.",
+            'address.required' => "Brugeren skal have en addresse.",
+            'address.digits_between' => "Addressens id skal være mellem 1 og 20 cifre.",
+            'role.required' => "Brugeren skal have en rolle.",
+            'role.digits_between' => "rollens id skal være mellem 1 og 20 cifre.",
+            'status.required' => "Brugeren skal have en status.",
+            'status.digits_between' => "Status iden skal være mellem 1 og 20 cifre.",
         ];
     }
 }
--- back-end API/OMUS_api/app/Http/Requests/UpdateUserRequest.php
+++ back-end API/OMUS_api/app/Http/Requests/UpdateUserRequest.php
@@ -11,7 +11,7 @@ class UpdateUserRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,33 @@ class UpdateUserRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'firstname' => "required|string|max:255",
+            'lastname' => "required|string|max:255",
+            'email' => "required|email:rfc",
+            'phoneNumber' => "required|string|max:11",
+            'address' => "required|integer|digits_between:1,20",
+            'role' => "required|integer|digits_between:1,20",
+            'status' => "required|integer|digits_between:1,20",
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'firstname.required' => "Brugeren skal have et fornavn, med mellemnavne.",
+            'firstname.max' => "Brugerens fornavn og mellemnavne kan ikke være længere end 255 tegn.",
+            'lastname.required' => "Brugeren skal have en efternavn.",
+            'lastname.max' => "Brugens efternavn kan ikke være længere end 255 tegn.",
+            'email.required' => "Brugeren skal have en email.",
+            'email.max' => "Brugerens email kan ikke være længere end 255 tegn.",
+            'phoneNumber.required' => "Brugerens skal have et telefon nummmer.",
+            'phoneNumber.max' => "Brugerens telefon nummer kan ikke være længere end 11 tegn.",
+            'address.required' => "Brugeren skal have en addresse.",
+            'address.digits_between' => "Addressens id skal være mellem 1 og 20 cifre.",
+            'role.required' => "Brugeren skal have en rolle.",
+            'role.digits_between' => "rollens id skal være mellem 1 og 20 cifre.",
+            'status.required' => "Brugeren skal have en status.",
+            'status.digits_between' => "Status iden skal være mellem 1 og 20 cifre.",
         ];
     }
 }
