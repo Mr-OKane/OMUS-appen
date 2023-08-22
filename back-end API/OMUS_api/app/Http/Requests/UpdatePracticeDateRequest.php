3a68007a54b58f411d155d99a85e0c60cccf3809
William Anker <William.s.anker@gmail.com>
2023-08-22 14:29:59 +0200

    completed the PracticeDate store and update requests v1.14.5

--- back-end API/OMUS_api/app/Http/Requests/UpdatePracticeDateRequest.php
+++ back-end API/OMUS_api/app/Http/Requests/UpdatePracticeDateRequest.php
@@ -11,7 +11,7 @@ class UpdatePracticeDateRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,15 @@ class UpdatePracticeDateRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'practiceDate' => "required|date_format:Y-m-d\TH:i"
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'practiceDate.required' => "øve datoen skal være valgt.",
+            'practiceDate.date_format' => "datoen skal være af format y-m-d H:i"
         ];
     }
 }
