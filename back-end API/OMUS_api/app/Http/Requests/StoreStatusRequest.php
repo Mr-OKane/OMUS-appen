0e3871001c6e2d61e5fbe298c00e45b9b379755b
William Anker <William.s.anker@gmail.com>
2023-08-22 14:28:02 +0200

    completed the Status store and update requests v1.18.5

--- back-end API/OMUS_api/app/Http/Requests/StoreStatusRequest.php
+++ back-end API/OMUS_api/app/Http/Requests/StoreStatusRequest.php
@@ -11,7 +11,7 @@ class StoreStatusRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,15 @@ class StoreStatusRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => "required|string|max:255"
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'name.required' => "statusen skal have et navn.",
+            'name.max' => "statusens navn kan ikke være længere end 255 tegn.",
         ];
     }
 }
--- back-end API/OMUS_api/app/Http/Requests/UpdateStatusRequest.php
+++ back-end API/OMUS_api/app/Http/Requests/UpdateStatusRequest.php
@@ -11,7 +11,7 @@ class UpdateStatusRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,15 @@ class UpdateStatusRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => "required|string|max:255"
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'name.required' => "statusen skal have et navn.",
+            'name.max' => "statusens navn kan ikke være længere end 255 tegn.",
         ];
     }
 }
