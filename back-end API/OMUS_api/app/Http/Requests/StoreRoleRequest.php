5bcf6c1acc715b0546e2d6ab28286005df462d64
William Anker <William.s.anker@gmail.com>
2023-08-22 14:29:00 +0200

    completed the role store and update requests v1.16.5

--- back-end API/OMUS_api/app/Http/Requests/StoreRoleRequest.php
+++ back-end API/OMUS_api/app/Http/Requests/StoreRoleRequest.php
@@ -11,7 +11,7 @@ class StoreRoleRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,15 @@ class StoreRoleRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => "required|string|max:255"
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'name.required' => "rollen skal have et navn.",
+            'name.max' => " rollens navn må max være 255 tegn."
         ];
     }
 }
