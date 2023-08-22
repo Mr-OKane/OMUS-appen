866106a609cbe52ba9955934ac20e28c9ac23ba6
William Anker <William.s.anker@gmail.com>
2023-08-22 14:36:30 +0200

    completed the Instrument store and update requests v1.8.5

--- back-end API/OMUS_api/app/Http/Requests/UpdateInstrumentRequest.php
+++ back-end API/OMUS_api/app/Http/Requests/UpdateInstrumentRequest.php
@@ -11,7 +11,7 @@ class UpdateInstrumentRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,15 @@ class UpdateInstrumentRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => 'required|string|max:255'
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'name.required' => 'Instrumentet skal have et navn',
+            'name.max' => 'Instrumentet må kun have et max af 255 tegn'
         ];
     }
 }
