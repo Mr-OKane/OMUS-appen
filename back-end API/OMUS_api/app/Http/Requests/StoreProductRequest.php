8e4ee7f9a29aec4a7f408b5afd5267a9d1cda942
William Anker <William.s.anker@gmail.com>
2023-08-22 14:29:30 +0200

    completed the Product store and update requests v1.15.5

--- back-end API/OMUS_api/app/Http/Requests/StoreProductRequest.php
+++ back-end API/OMUS_api/app/Http/Requests/StoreProductRequest.php
@@ -11,7 +11,7 @@ class StoreProductRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,22 @@ class StoreProductRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => "required|string|max:255",
+            'description' => "string|max:4000",
+            'image' => "image",
+            'price' => "required|decimal:2,2|max:10000000"
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'name.required' => "Produktet skal have et navn",
+            'name.max' => "produktets navn skal være mindere end 255 tegn",
+            'description.max' => "beskrivelsen skal mindre end 4000 tegn",
+            'image.image' => "billedet skal være af type jpg, jpeg eller png",
+            'price.required' => "produktet skal have en price.",
+            'price.max' => "prisen skal være mindre end en 1000000",
         ];
     }
 }
