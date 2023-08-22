13eb8dcfc9510de96ae1f6365a50b183e2b3f509
William Anker <William.s.anker@gmail.com>
2023-08-22 14:37:51 +0200

    completed the ChatRoom store and update requests v1.5.5

--- back-end API/OMUS_api/app/Http/Requests/StoreChatRoomRequest.php
+++ back-end API/OMUS_api/app/Http/Requests/StoreChatRoomRequest.php
@@ -11,7 +11,7 @@ class StoreChatRoomRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,14 @@ class StoreChatRoomRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => "required|string|max:255"
+        ];
+    }
+    public function messages()
+    {
+        return [
+            'name.required' => "chat rummet skal have navn.",
+            'name.max' => "chat rummet kan ikke være længere end 255 tegn",
         ];
     }
 }
