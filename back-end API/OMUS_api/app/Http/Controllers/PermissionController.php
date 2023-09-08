dc5894eedb8cc0347aeeb58735b19fe2ed656d88
William Anker <William.s.anker@gmail.com>
2023-09-08 13:55:21 +0200

    completed the Permission controller without permission checks v1.13.6

--- back-end API/OMUS_api/app/Http/Controllers/PermissionController.php
+++ back-end API/OMUS_api/app/Http/Controllers/PermissionController.php
@@ -5,15 +5,23 @@ namespace App\Http\Controllers;
 use App\Models\Permission;
 use App\Http\Requests\StorePermissionRequest;
 use App\Http\Requests\UpdatePermissionRequest;
+use Illuminate\Http\Request;
 
 class PermissionController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $paginationPerPage = $request->input('p') ?? 15;
+        if ($paginationPerPage >= 1000)
+        {
+            return response()->json(['message' => "1000+ permissions per page is to much"],400);
+        }
+        $permissions = Permission::paginate($paginationPerPage);
+
+        return response()->json(['object' => $permissions]);
     }
 
     /**
