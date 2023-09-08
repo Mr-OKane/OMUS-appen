1177a76c792f33f921f49e6fd95fd4f8aedfdbd4
William Anker <William.s.anker@gmail.com>
2023-09-08 13:26:25 +0200

    completed the basic ChatRoom controller without permission checks v1.5.6

--- back-end API/OMUS_api/app/Http/Controllers/ChatRoomController.php
+++ back-end API/OMUS_api/app/Http/Controllers/ChatRoomController.php
@@ -5,15 +5,24 @@ namespace App\Http\Controllers;
 use App\Models\ChatRoom;
 use App\Http\Requests\StoreChatRoomRequest;
 use App\Http\Requests\UpdateChatRoomRequest;
+use Illuminate\Http\Request;
+use newrelic\DistributedTracePayload;
 
 class ChatRoomController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $pagnationPerPage = $request->input('p') ?? 15;
+        if ($pagnationPerPage >= 1000){
+            return response()->json(['message' => "1000+ pagnation per page is to much"]);
+        }
+
+        $chatRoom = ChatRoom::withTrashed()->with('chats')->paginate($pagnationPerPage);
+
+        return response()->json(['object' => $chatRoom]);
     }
 
     /**
@@ -21,30 +30,54 @@ class ChatRoomController extends Controller
      */
     public function store(StoreChatRoomRequest $request)
     {
-        //
+        $request->validated();
+
+        $chatRoom = new ChatRoom();
+        $chatRoom['name'] = $request['name'];
+        $chatRoom->save();
+
+        $object = ChatRoom::withTrashed()->firstWhere('id','=',$chatRoom['id']);
+        $object->chats;
+
+        return response()->json(['message' => "created the chat room  successfully", 'object' => $object],201);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(ChatRoom $chatRoom)
+    public function show(string $chatRoom)
     {
-        //
+        $object = ChatRoom::withTrashed()->firstWhere('id','=',$chatRoom);
+        $object->chats;
+
+        return response()->json(['object' => $object]);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdateChatRoomRequest $request, ChatRoom $chatRoom)
+    public function update(UpdateChatRoomRequest $request, string $chatRoom)
     {
-        //
+        $request->validated();
+
+        $object = ChatRoom::withTrashed()->firstWhere('id','=',$chatRoom);
+        if ($object['name'] != $request['name'])
+        {
+            $object['name'] = $request['name'];
+        }
+
+        $object->save();
+
+        return response()->json(['message' => "updated the chat room successfully",'object' => $object]);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(ChatRoom $chatRoom)
+    public function destroy(string $chatRoom)
     {
-        //
+        $object = ChatRoom::withTrashed()->firstWhere('id','=',$chatRoom);
+        $object->delete();
+        return response()->json(['message' => "deleted the chat room successfully"]);
     }
 }
