330fc10225061901fddf149ea33325cbc9b67b7a
William Anker <William.s.anker@gmail.com>
2023-09-08 13:52:06 +0200

    completed the Notification controller without permission checks v1.10.6

--- back-end API/OMUS_api/app/Http/Controllers/NotificationController.php
+++ back-end API/OMUS_api/app/Http/Controllers/NotificationController.php
@@ -5,15 +5,24 @@ namespace App\Http\Controllers;
 use App\Models\Notification;
 use App\Http\Requests\StoreNotificationRequest;
 use App\Http\Requests\UpdateNotificationRequest;
+use Illuminate\Http\Request;
 
 class NotificationController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $paginationPerPage = $request->input('p') ?? 15;
+        if ($paginationPerPage >= 1000)
+        {
+            return response()->json(['message' => "1000+ pagination per page to much"],400);
+        }
+
+        $notifications = Notification::paginate($paginationPerPage);
+
+        return response()->json(['object' => $notifications]);
     }
 
     /**
@@ -21,30 +30,48 @@ class NotificationController extends Controller
      */
     public function store(StoreNotificationRequest $request)
     {
-        //
+        $request->validated();
+
+        $notification = new Notification();
+        $notification['message'] = $request['message'];
+        $notification->save();
+
+        return response()->json(['message' => "Created the notification successfully",'object' => $notification],201);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Notification $notification)
+    public function show(string $notification)
     {
-        //
+        $object = Notification::withTrashed()->firstWhere('id','=', $notification);
+        return response()->json(['object' => $object]);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdateNotificationRequest $request, Notification $notification)
+    public function update(UpdateNotificationRequest $request, string $notification)
     {
-        //
+        $request->validated();
+        $object = Notification::withTrashed()->firstWhere('id','=', $notification);
+
+        if ($object['message'] != $request['message'])
+        {
+            $object['message'] = $request['message'];
+        }
+        $object->save();
+
+        return response()->json(['message' => "Updated the notification successfully", 'object' => $object]);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Notification $notification)
+    public function destroy(string $notification)
     {
-        //
+        $object = Notification::withTrashed()->firstWhere('id','=', $notification);
+        $object->delete();
+        return response(['message' => "deleted the notification"]);
     }
 }
