72601d602ec37592d20dc136097ad789457eabad
William Anker <William.s.anker@gmail.com>
2023-09-08 13:28:25 +0200

    completed the City controller without permission checks v1.6.6

--- back-end API/OMUS_api/app/Http/Controllers/CityController.php
+++ back-end API/OMUS_api/app/Http/Controllers/CityController.php
@@ -5,15 +5,24 @@ namespace App\Http\Controllers;
 use App\Models\City;
 use App\Http\Requests\StoreCityRequest;
 use App\Http\Requests\UpdateCityRequest;
+use Illuminate\Http\Request;
 
 class CityController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $pagnationPerPage = $request->input('p') ?? 15;
+
+        if ($pagnationPerPage >= 1000)
+        {
+            return response()->json(['message' => "1000+ pagnation per page"],400);
+        }
+        $city = City::with('zipCodes.addresses')->paginate($pagnationPerPage);
+
+        return response()->json(['object' => $city]);
     }
 
     /**
@@ -21,30 +30,51 @@ class CityController extends Controller
      */
     public function store(StoreCityRequest $request)
     {
-        //
+        $request->validated();
+
+        $city = new City();
+        $city['city'] = $request['city'];
+        $city->save();
+
+        return response()->json(['message' => "created ethe city successfully",'object' => $city],201);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(City $city)
+    public function show(string $city)
     {
-        //
+        $object = City::withTrashed()->firstWhere('id','=',$city);
+        $object->zipCodes;
+        $object->zipCodes->addresses;
+
+        return response()->json(['object' => $object]);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdateCityRequest $request, City $city)
+    public function update(UpdateCityRequest $request, string $city)
     {
-        //
+        $request->validated();
+
+        $object = City::withTrashed()->firstWhere('id','=',$city);
+        if ($object['city'] != $request['city'])
+        {
+            $object['city'] = $request['city'];
+        }
+        $object->save();
+
+        return response()->json(['message' => "updated the cities name successfully", 'object' => $object],200);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(City $city)
+    public function destroy(string $city)
     {
-        //
+        $object = City::withTrashed()->firstWhere('id','=', $city);
+        $object->delete();
+        return response()->json(['message' => "deleted the city successfully"]);
     }
 }
