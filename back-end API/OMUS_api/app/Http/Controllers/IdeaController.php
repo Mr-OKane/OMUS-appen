08d9f9d44aee8757209decb348bbaa82dbf18bef
William Anker <William.s.anker@gmail.com>
2023-09-08 13:28:45 +0200

    completed the Idea controller without permission checks v1.7.6

--- back-end API/OMUS_api/app/Http/Controllers/IdeaController.php
+++ back-end API/OMUS_api/app/Http/Controllers/IdeaController.php
@@ -5,15 +5,22 @@ namespace App\Http\Controllers;
 use App\Models\Idea;
 use App\Http\Requests\StoreIdeaRequest;
 use App\Http\Requests\UpdateIdeaRequest;
+use Illuminate\Http\Request;
 
 class IdeaController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $pagnationPerPage = $request->input('p') ?? 15;
+        if ($pagnationPerPage >= 1000){
+            return response()->json(['message' => ""],400);
+        }
+        $ideas = Idea::paginate($pagnationPerPage);
+
+        return response()->json(['object' => $ideas]);
     }
 
     /**
@@ -21,30 +28,46 @@ class IdeaController extends Controller
      */
     public function store(StoreIdeaRequest $request)
     {
-        //
+        $request->validated();
+
+        $idea = new Idea();
+        $idea['idea'] = $request['idea'];
+        $idea->save();
+
+        return response()->json(['message' => "created the Idea successfully", 'object' => $idea],201);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Idea $idea)
+    public function show(string $idea)
     {
-        //
+        $object = Idea::withTrashed()->firstWhere('id','=', $idea);
+        return response()->json(['object' => $object]);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdateIdeaRequest $request, Idea $idea)
+    public function update(UpdateIdeaRequest $request, string $idea)
     {
-        //
+        $request->validated();
+
+        $object = Idea::withTrashed()->firstWhere('id','=',$idea);
+        if ($object['idea'] != $request['idea']){
+            $object['idea'] = $request['idea'];
+        }
+        $object->save();
+
+        return response()->json(['message' => "updated the idea successfully",'object' => $object]);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Idea $idea)
+    public function destroy(string $idea)
     {
-        //
+        $object = Idea::withTrashed()->firstWhere('id','=', $idea);
+        return response()->json(['message' => "deleted the idea"]);
     }
 }
