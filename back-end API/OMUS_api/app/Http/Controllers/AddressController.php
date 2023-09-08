1283230894f1328a4cf3df05e72bf705b95e376b
William Anker <William.s.anker@gmail.com>
2023-09-08 13:18:50 +0200

    completed the Address controller without permission checks v1.2.6

--- back-end API/OMUS_api/app/Http/Controllers/AddressController.php
+++ back-end API/OMUS_api/app/Http/Controllers/AddressController.php
@@ -5,15 +5,23 @@ namespace App\Http\Controllers;
 use App\Models\Address;
 use App\Http\Requests\StoreAddressRequest;
 use App\Http\Requests\UpdateAddressRequest;
+use Illuminate\Http\Request;
+use function Termwind\ask;
 
 class AddressController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $paginationPerPage = $request->input('p') ?? 15;
+        if ($paginationPerPage >= 1000){
+            return response()->json(['message' => "to high of pagination"],400);
+        }
+        $address = Address::with('zipCode.city')->paginate($paginationPerPage);
+
+        return response()->json(['object' => $address]);
     }
 
     /**
@@ -21,30 +29,60 @@ class AddressController extends Controller
      */
     public function store(StoreAddressRequest $request)
     {
-        //
+        $request->validated();
+
+        $address = new Address();
+        $address['address'] = $request["address"];
+        $address->zipCode()->associate($request['zipCode']);
+        $address->save();
+
+        $object = Address::withTrashed()->where('id', '=', $address['id'])->first();
+        $object->zipCode;
+        $object->zipCode->city;
+
+        return response()->json(['message' => "created the address successfully",'object' => $object],201);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Address $address)
+    public function show(string $address)
     {
-        //
+        $object = Address::withTrashed()->where('id', '=', $address)->first();
+        $object->zipCode;
+        $object->zipCode->city;
+
+        return response()->json(['object' => $object]);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdateAddressRequest $request, Address $address)
+    public function update(UpdateAddressRequest $request, string $address)
     {
-        //
+        $request->validated();
+
+        $object = Address::withTrashed()->where('id', '=', $address)->first();
+
+        if ($object['address'] != $request['address'])
+        {
+            $object['address'] = $request['address'];
+        }
+        $object->zipCode()->associate($request['zipCode']);
+        $object->save();
+
+        return response()->json(['message' => "updated the address",'object' => $object]);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Address $address)
+    public function destroy(string $address)
     {
-        //
+        $object = Address::withTrashed()->firstWhere('id','=', $address);
+        $object->delete();
+        return response()->json(['message' => "deleted the address"]);
     }
+
+
 }
