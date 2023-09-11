68ae89430be6df38af2e66fe36dc0fc135ed2cd4
William Anker <William.s.anker@gmail.com>
2023-09-11 08:12:35 +0200

    completed the Practice date Controller without permission check v1.14.6

--- back-end API/OMUS_api/app/Http/Controllers/PracticeDateController.php
+++ back-end API/OMUS_api/app/Http/Controllers/PracticeDateController.php
@@ -5,15 +5,21 @@ namespace App\Http\Controllers;
 use App\Models\PracticeDate;
 use App\Http\Requests\StorePracticeDateRequest;
 use App\Http\Requests\UpdatePracticeDateRequest;
+use Illuminate\Http\Request;
 
 class PracticeDateController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $pagnationPerPage = $request->input('p') ?? 15;
+        if ($pagnationPerPage >= 1000)
+        {
+            return response()->json(['message' => "1000+ pagination per page is to much"],400);
+        }
+        $practiceDates = PracticeDate::pagination($pagnationPerPage);
     }
 
     /**
@@ -21,30 +27,50 @@ class PracticeDateController extends Controller
      */
     public function store(StorePracticeDateRequest $request)
     {
-        //
+        $request->validated();
+
+        $practiceDate = new PracticeDate();
+        $practiceDate['practice_date'] = $request['practiceDate'];
+        $practiceDate->save();
+
+        return response()->json(['message' => "created the practice date successfully",'object' => $practiceDate],201);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(PracticeDate $practiceDate)
+    public function show(string $practiceDate)
     {
-        //
+        $object = PracticeDate::withTrashed()->firstWhere('id','=', $practiceDate);
+        $object->users;
+        $object->cakeArrengements;
+        return response()->json(['object' => $object]);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdatePracticeDateRequest $request, PracticeDate $practiceDate)
+    public function update(UpdatePracticeDateRequest $request, string $practiceDate)
     {
-        //
+        $request->validated();
+        $object = PracticeDate::withTrashed()->firstWhere('id','=', $practiceDate);
+
+        if ($object['practice_date'] != $request['practiceDate'])
+        {
+            $object['practice_date'] = $request['practiceDate'];
+        }
+        $object->save();
+
+        return response()->json(['message' => "updated the practice date successfully.",'object' => $object]);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(PracticeDate $practiceDate)
+    public function destroy(string $practiceDate)
     {
-        //
+        $object = PracticeDate::withTrashed()->firstWhere('id','=', $practiceDate);
+        $object->delete();
+        return response()->json(['message' => "deleted the practice date successfully"]);
     }
 }
