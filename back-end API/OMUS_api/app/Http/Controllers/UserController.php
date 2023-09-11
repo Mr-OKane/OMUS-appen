a752925bcdad64dd507ce091e48eee5a2539fda7
William Anker <William.s.anker@gmail.com>
2023-09-11 08:25:05 +0200

    completed the basic User Controller without permission check v1.20.6

--- back-end API/OMUS_api/app/Http/Controllers/UserController.php
+++ back-end API/OMUS_api/app/Http/Controllers/UserController.php
@@ -2,6 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreUserRequest;
+use App\Http\Requests\UpdateUserRequest;
+use App\Models\Address;
+use App\Models\City;
+use App\Models\User;
+use App\Models\ZipCode;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
@@ -9,40 +15,154 @@ class UserController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $paginationPerPage = $request->input() ?? 15;
+        if ($paginationPerPage >= 1000)
+        {
+            return response()->json(['message' => "1000+ Users per page is to much"],400);
+        }
+        $users = User::with('address.zipcode.city')->with('role')
+            ->with('status')->with('teams')->with('instruments')
+            ->paginate($paginationPerPage);
+
+        return  response()->json(['obejct' => $users]);
+
     }
 
+    public function userAddress(User $user, Request $request)
+    {
+
+        $address = Address::withoutTrashed()->firstWhere('address','=', $request['address']);
+        $zipCode = ZipCode::withoutTrashed()->firstWhere('zip_code','=', $request['zipCode']);
+        $city = City::withoutTrashed()->firstWhere('city','=', $request['city']);
+
+        if (empty($address))
+        {
+            $newAddress = new Address();
+            $newAddress['address'] = $request['address'];
+            if (empty($zipCode))
+            {
+                $newZipCode = new ZipCode();
+                $newZipCode['zip_code'] = $request['zipCode'];
+                if (empty($city))
+                {
+                    $newCity = new City();
+                    $newCity['city'] = $request['city'];
+                    $newCity->save();
+
+                    $newZipCode->city()->associate($newCity['id']);
+                }else
+                {
+                    $newZipCode->city()->associate($city['id']);
+                }
+                $newZipCode->save();
+
+                $newAddress->zipCode()->associate($newZipCode['id']);
+            }else
+            {
+                $newAddress->zipCode()->associate($zipCode['id']);
+            }
+        }else
+        {
+            $user->address()->associate($request['address']);
+        }
+    }
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreUserRequest $request)
     {
-        //
+        $request->validated();
+        $search = User::withTrashed()->firstWhere('email', '=', $request['email']);
+
+        if (!empty($search))
+        {
+            return response()->json(['message' => "the user already exist"],400);
+        }
+
+        $user = new User();
+        $user['first_name'] = $request['firstname'];
+        $user['last_name'] = $request['lastname'];
+        $user['phone_nr'] = $request['phoneNumber'];
+        $user['email'] = $request['email'];
+        $user['password'] = $request['password'];
+        $this->userAddress($user, $request);
+        $user->role()->associate($request['role']);
+        $user->status()->associate($request['status']);
+        $user->save();
+
+        $object = User::withTrashed()->firstWhere('id','=', $user['id']);
+        $object->address->zipCode->city;
+        $object->role;
+        $object->status;
+
+        return response()->json(['message' => "created the user successfully", 'object' => $object],201);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(string $user)
     {
-        //
+       $object = User::withTrashed()->firstWhere('id','=', $user);
+       $object->address->zipCode->city;
+       $object->role;
+       $object->status;
+
+       return response()->json(['object' => $object]);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(UpdateUserRequest $request, string $user)
     {
-        //
+        $request->validated();
+
+        $search = User::withTrashed()->firstWhere('email', '=', $request['email']);
+        $object = User::withTrashed()->firstWhere('id','=', $user);
+
+        if (!empty($search))
+        {
+            return response()->json(['message' => "the user already exist"],400);
+        }
+
+        if ($object['first_name'] != $request['firstname'])
+        {
+            $object['first_name'] != $request['firstname'];
+        }
+        if ($object['last_name'] != $request['lastname'])
+        {
+            $object['last_name'] != $request['lastname'];
+        }
+        if ($object['phone_nr'] != $request['phoneNumber'])
+        {
+            $object['phone_nr'] = $request['phoneNumber'];
+        }
+        if ($object['email'] != $request['email'])
+        {
+            $object['email'] = $request['email'];
+        }
+        $object->role()->associate($request['role']);
+        $object->status()->associate($request['status']);
+        $this->userAddress($object, $request);
+        $object->save();
+
+        $object->address->zipCode->city;
+        $object->role;
+        $object->status;
+
+        return response()->json(['message' => "Updated the user successfully.", 'object' => $object]);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(string $user)
     {
-        //
+        $object = User::withTrashed()->firstWhere('id','=', $user);
+        $object->delete();
+        return response()->json(['message' => "deleted the user successfully."]);
     }
 }
