a1fc55b59f505cf5a2e092287499290f94608d66
William Anker <William.s.anker@gmail.com>
2023-09-11 08:45:08 +0200

    Modified the Order store and update requests v1.11.5

--- back-end API/OMUS_api/app/Http/Requests/StoreOrderRequest.php
+++ back-end API/OMUS_api/app/Http/Requests/StoreOrderRequest.php
@@ -25,7 +25,8 @@ class StoreOrderRequest extends FormRequest
             'address' => "required|integer|digits_between:1,20",
             'status' => "required|integer|digits_between:1,20",
             'user' => "required|integer|digits_between:1,20",
-            'orderDate' => "required|date_format:Y-m-d\TH:i"
+            'products' => "required|array:product_id",
+            'products.*' => "integer",
         ];
     }
 
@@ -37,8 +38,6 @@ class StoreOrderRequest extends FormRequest
             'status.required' => "ordren skal have en status.",
             'status.digits_between' => "ordrens status skal være mellem 1 og 20 cifre.",
             'user.required' => "ordren skal have en bruger.",
-            'orderDate.required' => "orderen skal have en dato på hvornår den er købt.",
-            'orderDate.date_format' => "datoen skal være en datetime."
         ];
     }
 
