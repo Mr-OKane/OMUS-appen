828dab8eddc5cceb05dfa3801e28a5fad5d101b5
William Anker <William.s.anker@gmail.com>
2023-09-11 08:24:32 +0200

    completed the basic Team Controller without permission check v1.19.6

--- back-end API/OMUS_api/app/Http/Controllers/TeamController.php
+++ back-end API/OMUS_api/app/Http/Controllers/TeamController.php
@@ -5,15 +5,25 @@ namespace App\Http\Controllers;
 use App\Models\Team;
 use App\Http\Requests\StoreTeamRequest;
 use App\Http\Requests\UpdateTeamRequest;
+use Illuminate\Http\Request;
+
 
 class TeamController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $paginationPerPage = $request->input('p') ?? 15;
+        if ($paginationPerPage >= 1000)
+        {
+            return response()->json(['message' => "1000+ is to much pagination"],400);
+        }
+
+        $teams = Team::with('users')->paginate($paginationPerPage);
+
+        return response()->json(['object' => $teams]);
     }
 
     /**
@@ -21,30 +31,62 @@ class TeamController extends Controller
      */
     public function store(StoreTeamRequest $request)
     {
-        //
+        $request->validated();
+
+        $search = Team::withTrashed()->firstWhere('name','=', $request['name']);
+        if (!empty($search))
+        {
+            return response()->json(['message' => "the team name already exists"],400);
+        }
+
+        $team = new Team();
+        $team['name'] = $request['name'];
+        $team->save();
+
+        return response()->json(['message' => "Created the team successfully", 'object' => $team],201);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Team $team)
+    public function show(string $team)
     {
-        //
+        $object = Team::withTrashed()->firstWhere('id','=', $team);
+        $object->users;
+
+        return response()->json(['object' => $object]);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdateTeamRequest $request, Team $team)
+    public function update(UpdateTeamRequest $request, string $team)
     {
-        //
+        $request->validated();
+        $object = Team::withTrashed()->firstWhere('id','=', $team);
+
+        if ($object['name'] != $request['name'])
+        {
+            $search = Team::withTrashed()->firstWhere('name','=', $request['name']);
+            if (!empty($search))
+            {
+                return response()->json(['there is a team with that name'],400);
+            }
+            $object['name'] =  $request['name'];
+        }
+        $object->save();
+
+        return response()->json(['message' => "Updated the team successfully", 'object' => $object]);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Team $team)
+    public function destroy(string $team)
     {
-        //
+        $object = Team::withTrashed()->firstWhere('id','=', $team);
+        $object->delete();
+
+        return response()->json(['message' => "deleted the Team successfully"]);
     }
 }
