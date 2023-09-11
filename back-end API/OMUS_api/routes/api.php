45e324b47fa0a3dfc8a94a3d0bdfc668de588672
William Anker <William.s.anker@gmail.com>
2023-09-11 12:45:03 +0200

    Created all the basic routes in the api route file v1.23.7

--- back-end API/OMUS_api/routes/api.php
+++ back-end API/OMUS_api/routes/api.php
@@ -2,6 +2,26 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AddressController;
+use App\Http\Controllers\CakeArrengementController;
+use App\Http\Controllers\ChatController;
+use App\Http\Controllers\ChatRoomController;
+use App\Http\Controllers\CityController;
+use App\Http\Controllers\IdeaController;
+use App\Http\Controllers\InstrumentController;
+use App\Http\Controllers\MessageController;
+use App\Http\Controllers\NotificationController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\OrderProductController;
+use App\Http\Controllers\PermissionController;
+use App\Http\Controllers\PracticeDateController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\RoleController;
+use App\Http\Controllers\SheetController;
+use App\Http\Controllers\StatusController;
+use App\Http\Controllers\TeamController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ZipCodeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +36,31 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::post('authenticaiton',[\App\Http\Controllers\Auth\LoginController::class,'authentiacte']);
+
+Route::middleware('auth:sanctum')->group(function (){
+
+    Route::get('logout',[\App\Http\Controllers\Auth\LoginController::class,'logout'])->name('logout');
+
+    Route::apiResource('addresses',AddressController::class);
+    Route::apiResource('cakeArrengements', CakeArrengementController::class);
+    Route::apiResource('chats', ChatController::class);
+    Route::apiResource('chatRooms',ChatRoomController::class);
+    Route::apiResource('cities',CityController::class);
+    Route::apiResource('ideas',IdeaController::class);
+    Route::apiResource('instruments',InstrumentController::class);
+    Route::apiResource('messages',MessageController::class);
+    Route::apiResource('notifications',NotificationController::class);
+    Route::apiResource('orders',OrderController::class);
+    Route::apiResource('permissions',PermissionController::class);
+    Route::apiResource('practiceDates',PracticeDateController::class);
+    Route::apiResource('products',ProductController::class);
+    Route::apiResource('roles', RoleController::class);
+    Route::apiResource('sheets',SheetController::class);
+    Route::apiResource('statuses',StatusController::class);
+    Route::apiResource('teams',TeamController::class);
+    Route::apiResource('users',UserController::class);
+    Route::apiResource('zipCodes',ZipCodeController::class);
 });
