34571bd91562156932ec53ba23d7e05da6b0a3df
William Anker <William.s.anker@gmail.com>
2023-09-28 12:42:12 +0200

    Completed the CakeArrangement Policy v1.3.7

---
+++ back-end API/OMUS_api/app/Policies/CakeArrangementPolicy.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\CakeArrangement;
+use App\Models\Permission;
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+
+class CakeArrangementPolicy
+{
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny(): Response
+    {
+        $user = auth()->user();
+        return $user->role->permissions->contains(Permission::withTrashed()->firstWhere('name','=',"cakeArrangement.viewAny"))
+            ? Response::allow()
+            : Response::deny('You ar not the chosen one',403);
+    }
+
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user): Response
+    {
+        return $user->role->permissions->contains(Permission::withTrashed()->firstWhere('name','=',"cakeArrangement.view"))
+            ? Response::allow()
+            : Response::deny('You are not the chosen one',403);
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user): Response
+    {
+        return $user->role->permissions->contains(Permission::withTrashed()->firstWhere('name','=',"cakeArrangement.create"))
+            ? Response::allow()
+            : Response::deny('You are not the chosen one',403);
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user): Response
+    {
+        $authUser = auth('sanctum')->user();
+        return ($authUser['id'] === $user['id'] || $authUser->role->permissions->contains(Permission::withTrashed()
+                ->firstWhere('name','=',"cakeArrangement.update")))
+            ? Response::allow()
+            : Response::deny('You are not the chosen one',403);
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, CakeArrangement $CakeArrangement): Response
+    {
+
+        return $user->role->permissions->contains(Permission::withTrashed()->firstWhere('name','=',"cakeArrangement.delete"))
+            ? Response::allow()
+            : Response::deny('You are not the chosen one',403);
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     */
+    public function restore(User $user, CakeArrangement $CakeArrangement)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     */
+    public function forceDelete(User $user, CakeArrangement $CakeArrangement)
+    {
+        //
+    }
+}
