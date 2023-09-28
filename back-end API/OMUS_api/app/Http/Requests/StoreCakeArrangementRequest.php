ed0abf8247b4cc08536674a517c45d98866bfbed
William Anker <William.s.anker@gmail.com>
2023-09-28 14:20:26 +0200

    renamed the CakeArrangement store and update requests v1.3.5.1

--- back-end API/OMUS_api/app/Http/Requests/StoreCakeArrangementRequest.php
+++ back-end API/OMUS_api/app/Http/Requests/StoreCakeArrangementRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class StoreCakeArrengementRequest extends FormRequest
+class StoreCakeArrangementRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
