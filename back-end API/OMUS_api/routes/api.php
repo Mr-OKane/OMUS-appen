02ebc3a3b7cea7e2df0a354a6a75d2c931a6d461
William Anker <William.s.anker@gmail.com>
2023-09-28 15:07:05 +0200

    changed all restore routes to get and added a message route in the Api route v1.23.7.1

--- back-end API/OMUS_api/routes/api.php
+++ back-end API/OMUS_api/routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AddressController;
-use App\Http\Controllers\CakeArrengementController;
+use App\Http\Controllers\CakeArrangementController;
 use App\Http\Controllers\ChatController;
 use App\Http\Controllers\ChatRoomController;
 use App\Http\Controllers\CityController;
@@ -17,7 +17,6 @@ use App\Http\Controllers\PracticeDateController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\RoleController;
 use App\Http\Controllers\SheetController;
-use App\Http\Controllers\StatusController;
 use App\Http\Controllers\TeamController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\ZipCodeController;
@@ -33,11 +32,11 @@ use App\Http\Controllers\ZipCodeController;
 |
 */
 
+Route::post('login',[\App\Http\Controllers\Auth\LoginController::class,'login']);
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-Route::post('authenticaiton',[\App\Http\Controllers\Auth\LoginController::class,'authentiacte']);
 
 Route::middleware('auth:sanctum')->group(function (){
 
@@ -45,66 +44,64 @@ Route::middleware('auth:sanctum')->group(function (){
 
     // address
     Route::get('addresses/deleted',[AddressController::class, 'deleted']);
-    Route::post('addresses/{id}/restore',[AddressController::class, 'restore']);
+    Route::get('addresses/{id}/restore',[AddressController::class, 'restore']);
     Route::delete('addresses/{id}/forceDelete', [AddressController::class, 'forceDelete']);
 
     // Chat
     Route::get('chats/deleted', [ChatController::class, 'deleted']);
-    Route::post('chats/{id}/restore', [ChatController::class, 'restore']);
+    Route::get('chats/{id}/restore', [ChatController::class, 'restore']);
     Route::delete('chats/{id}/forceDelete', [ChatController::class, 'forceDelete']);
 
     // Chat room
     Route::get('chat-rooms/deleted', [ChatRoomController::class, 'deleted']);
-    Route::post('chat-rooms/{id}/restore', [ChatRoomController::class, 'restore']);
+    Route::get('chat-rooms/{id}/restore', [ChatRoomController::class, 'restore']);
     Route::delete('chat-rooms/{id}/forceDelete', [ChatRoomController::class, 'forceDelete']);
 
     // city
     Route::get('cities/deleted',[CityController::class, 'deleted']);
-    Route::post('cities/{id}/restore',[CityController::class, 'restore']);
+    Route::get('cities/{id}/restore',[CityController::class, 'restore']);
     Route::delete('cities/{id}/forceDelete',[CityController::class,'forceDelete']);
 
     // Instrument
     Route::get('instruments/deleted',[InstrumentController::class,'deleted']);
-    Route::post('instruments/{id}/restore',[InstrumentController::class, 'restore']);
+    Route::get('instruments/{id}/restore',[InstrumentController::class, 'restore']);
     Route::delete('instruments/{id}/forceDelete',[InstrumentController::class, 'forceDelete']);
+
+    // Message
+    Route::get('messages/{chat_id}',[MessageController::class,'chatMessages']);
 
     // Order
     Route::get('orders/deleted',[OrderController::class, 'deleted']);
-    Route::post('orders/{id}/restore',[OrderController::class, 'restore']);
+    Route::get('orders/{id}/restore',[OrderController::class, 'restore']);
     Route::delete('orders/{id}/forceDelete',[OrderController::class, 'forceDelete']);
 
     // Product
     Route::get('products/deleted',[ProductController::class,'deleted']);
-    Route::post('products/{id}/restore',[ProductController::class,'restore']);
+    Route::get('products/{id}/restore',[ProductController::class,'restore']);
     Route::delete('products/{id}/forceDelete',[ProductController::class,'forceDelete']);
 
     // Role
     Route::get('roles/deleted', [RoleController::class,'deleted']);
-    Route::post('roles/{id}/restore', [RoleController::class, 'restore']);
+    Route::get('roles/{id}/restore', [RoleController::class, 'restore']);
     Route::delete('roles/{id}/forceDelete', [RoleController::class, 'forceDelete']);
 
     // Sheet
     Route::get('sheets/deleted', [SheetController::class,'deleted']);
-    Route::post('sheets/{id}/restore', [SheetController::class,'restore']);
+    Route::get('sheets/{id}/restore', [SheetController::class,'restore']);
     Route::delete('sheets/{id}/forceDelete', [SheetController::class, 'forceDelete']);
-
-    // Status
-    Route::get('statuses/deleted',[StatusController::class,'deleted']);
-    Route::post('statuses/{id}/restore',[StatusController::class,'restore']);
-    Route::delete('statuses/{id}/forceDelete',[StatusController::class,'forceDelete']);
 
     // Team
     Route::get('teams/deleted', [TeamController::class,'deleted']);
-    Route::post('teams/{id}/restore', [TeamController::class,'restore']);
+    Route::get('teams/{id}/restore', [TeamController::class,'restore']);
     Route::delete('teams/{id}/forceDelete',[TeamController::class,'forceDelete']);
 
     // User
     Route::get('users/deleted',[UserController::class, 'deleted']);
-    Route::post('users/{id}/restore',[UserController::class, 'restore']);
+    Route::get('users/{id}/restore',[UserController::class, 'restore']);
     Route::delete('users/{id}/forceDelete',[UserController::class,'forceDelete']);
 
     Route::apiResource('addresses',AddressController::class);
-    Route::apiResource('cake-arrengements', CakeArrengementController::class);
+    Route::apiResource('cake-arrangements', CakeArrangementController::class);
     Route::apiResource('chats', ChatController::class);
     Route::apiResource('chat-rooms',ChatRoomController::class);
     Route::apiResource('cities',CityController::class);
@@ -118,7 +115,6 @@ Route::middleware('auth:sanctum')->group(function (){
     Route::apiResource('products',ProductController::class);
     Route::apiResource('roles', RoleController::class);
     Route::apiResource('sheets',SheetController::class);
-    Route::apiResource('statuses',StatusController::class);
     Route::apiResource('teams',TeamController::class);
     Route::apiResource('users',UserController::class);
     Route::apiResource('zip-codes',ZipCodeController::class);
