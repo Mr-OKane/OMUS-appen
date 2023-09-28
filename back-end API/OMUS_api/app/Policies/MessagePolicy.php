0c8e4843d527d76d91d9e5eefa0ee8aa8ccfa84a
William Anker <William.s.anker@gmail.com>
2023-09-28 12:46:00 +0200

    Completed the Message Policy v1.9.7

--- back-end API/OMUS_api/app/Policies/MessagePolicy.php
+++ back-end API/OMUS_api/app/Policies/MessagePolicy.php
@@ -3,6 +3,7 @@
 namespace App\Policies;
 
 use App\Models\Message;
+use App\Models\Permission;
 use App\Models\User;
 use Illuminate\Auth\Access\Response;
 
@@ -11,47 +12,58 @@ class MessagePolicy
     /**
      * Determine whether the user can view any models.
      */
-    public function viewAny(User $user): bool
+    public function viewAny(): Response
     {
-        //
+        $user = auth('sanctum')->user();
+        return $user->role->permissions->contains(Permission::withTrashed()->firstWhere('name','=','message.viewAny'))
+            ? Response::allow()
+            : Response::deny('You are not the chosen one',403);
     }
 
     /**
      * Determine whether the user can view the model.
      */
-    public function view(User $user, Message $message): bool
+    public function view(User $user): Response
     {
-        //
+        return $user->role->permissions->contains(Permission::withTrashed()->firstWhere('name','=','message.view'))
+            ? Response::allow()
+            : Response::deny('You are not the chosen one',403);
     }
 
     /**
      * Determine whether the user can create models.
      */
-    public function create(User $user): bool
+    public function create(User $user): Response
     {
-        //
+        return $user->role->permissions->contains(Permission::withTrashed()->firstWhere('name','=','message.create'))
+            ? Response::allow()
+            : Response::deny('You are not the chosen one',403);
     }
 
     /**
      * Determine whether the user can update the model.
      */
-    public function update(User $user, Message $message): bool
+    public function update(User $user): Response
     {
-        //
+        return $user->role->permissions->contains(Permission::withTrashed()->firstWhere('name','=','message.update'))
+            ? Response::allow()
+            : Response::deny('You are not the chosen one',403);
     }
 
     /**
      * Determine whether the user can delete the model.
      */
-    public function delete(User $user, Message $message): bool
+    public function delete(User $user): Response
     {
-        //
+        return $user->role->permissions->contains(Permission::withTrashed()->firstWhere('name','=','message.delete'))
+            ? Response::allow()
+            : Response::deny('You are not the chosen one',403);
     }
 
     /**
      * Determine whether the user can restore the model.
      */
-    public function restore(User $user, Message $message): bool
+    public function restore(User $user, Message $message)
     {
         //
     }
@@ -59,7 +71,7 @@ class MessagePolicy
     /**
      * Determine whether the user can permanently delete the model.
      */
-    public function forceDelete(User $user, Message $message): bool
+    public function forceDelete(User $user, Message $message)
     {
         //
     }
