12a692db01cbd643e92ee4eb2b55982f1be45aa6
William Anker <William.s.anker@gmail.com>
2023-09-28 12:43:25 +0200

    Completed the City Policy v1.6.7

--- back-end API/OMUS_api/app/Policies/CityPolicy.php
+++ back-end API/OMUS_api/app/Policies/CityPolicy.php
@@ -3,6 +3,7 @@
 namespace App\Policies;
 
 use App\Models\City;
+use App\Models\Permission;
 use App\Models\User;
 use Illuminate\Auth\Access\Response;
 
@@ -11,56 +12,84 @@ class CityPolicy
     /**
      * Determine whether the user can view any models.
      */
-    public function viewAny(User $user): bool
+    public function viewAny(): Response
     {
-        //
+        $user = auth('sanctum')->user();
+        return $user->role->permissions->contains(Permission::withTrashed()->firstWhere('name','=','city.viewAny'))
+            ? Response::allow()
+            : Response::deny('You are not the chosen one',403);
+    }
+
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny_deleted(): Response
+    {
+        $user = auth('sanctum')->user();
+        return $user->role->permissions->contains(Permission::withTrashed()->firstWhere('name','=','city.viewAny.deleted'))
+            ? Response::allow()
+            : Response::deny('You are not the chosen one',403);
     }
 
     /**
      * Determine whether the user can view the model.
      */
-    public function view(User $user, City $city): bool
+    public function view(User $user): Response
     {
-        //
+        return $user->role->permissions->contains(Permission::withTrashed()->firstWhere('name','=','city.view'))
+            ? Response::allow()
+            : Response::deny('You are not the chosen one',403);
     }
 
     /**
      * Determine whether the user can create models.
      */
-    public function create(User $user): bool
+    public function create(User $user): Response
     {
-        //
+        return $user->role->permissions->contains(Permission::withTrashed()->firstWhere('name','=','city.create'))
+            ? Response::allow()
+            : Response::deny('You are not the chosen one',403);
     }
 
     /**
      * Determine whether the user can update the model.
      */
-    public function update(User $user, City $city): bool
+    public function update(User $user): Response
     {
-        //
+        $authUser = auth('sanctum')->user();
+        return ($authUser['id'] === $user['id'] || $authUser->role->permissions->contains(Permission::withTrashed()
+                ->firstWhere('name','=','city.update')))
+            ? Response::allow()
+            : Response::deny('You are not the chosen one',403);
     }
 
     /**
      * Determine whether the user can delete the model.
      */
-    public function delete(User $user, City $city): bool
+    public function delete(User $user): Response
     {
-        //
+        return $user->role->permissions->contains(Permission::withTrashed()->firstWhere('name','=','city.delete'))
+            ? Response::allow()
+            : Response::deny('You are not the chosen one',403);
     }
 
     /**
      * Determine whether the user can restore the model.
      */
-    public function restore(User $user, City $city): bool
+    public function restore(User $user): Response
     {
-        //
+        return $user->role->permissions->contains(Permission::withTrashed()->firstWhere('name','=','city.restore'))
+            ? Response::allow()
+            : Response::deny('You are not the chosen one',403);
     }
 
     /**
      * Determine whether the user can permanently delete the model.
      */
-    public function forceDelete(User $user, City $city): bool
+    public function forceDelete(User $user): Response
     {
-        //
+        return $user->role->permissions->contains(Permission::withTrashed()->firstWhere('name','=','city.force.delete'))
+            ? Response::allow()
+            : Response::deny('You are not the chosen one',403);
     }
 }
