908728c6e8b6c125864a91926820b081d27b517a
William Anker <William.s.anker@gmail.com>
2023-09-28 14:30:58 +0200

    Completed the Address Seeder v1.2.3.1

--- back-end API/OMUS_api/database/seeders/AddressSeeder.php
+++ back-end API/OMUS_api/database/seeders/AddressSeeder.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Address;
+use App\Models\ZipCode;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use League\Csv\Reader;
 
 class AddressSeeder extends Seeder
 {
@@ -12,6 +15,15 @@ class AddressSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $csv = Reader::createFromPath(storage_path('CSV\adresser.csv'),'r')
+            ->setHeaderOffset(0);
+        $records = $csv->getRecords();
+
+        foreach ($records as $offset => $record){
+            Address::firstOrCreate([
+                'zip_code_id' => ZipCode::where('zip_code','=',$record['postnr'])->first()->id,
+                'address' => $record['vejnavn'].' '.$record['husnr'],
+            ]);
+        }
     }
 }
