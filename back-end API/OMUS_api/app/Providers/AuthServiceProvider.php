fca1208506c4db3381fd05636b6e2396b3bf2e6c
William Anker <William.s.anker@gmail.com>
2023-09-28 15:11:24 +0200

    Linked all the models to a policy and registered the policies in the AuthServiceProvider file v1.22.7

--- back-end API/OMUS_api/app/Providers/AuthServiceProvider.php
+++ back-end API/OMUS_api/app/Providers/AuthServiceProvider.php
@@ -3,7 +3,41 @@
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
+use App\Models\Address;
+use App\Models\Chat;
+use App\Models\ChatRoom;
+use App\Models\City;
+use App\Models\Idea;
+use App\Models\Instrument;
+use App\Models\Message;
+use App\Models\Notification;
+use App\Models\Order;
+use App\Models\PracticeDate;
+use App\Models\Product;
+use App\Models\Role;
+use App\Models\Status;
+use App\Models\Team;
+use App\Models\User;
+use App\Models\ZipCode;
+use App\Policies\AddressPolicy;
+use App\Policies\ChatPolicy;
+use App\Policies\ChatRoomPolicy;
+use App\Policies\CityPolicy;
+use App\Policies\IdeaPolicy;
+use App\Policies\InstrumentPolicy;
+use App\Policies\MessagePolicy;
+use App\Policies\NotificationPolicy;
+use App\Policies\OrderPolicy;
+use App\Policies\PracticeDatePolicy;
+use App\Policies\ProductPolicy;
+use App\Policies\RolePolicy;
+use App\Policies\StatusPolicy;
+use App\Policies\TeamPolicy;
+use App\Policies\UserPolicy;
+use App\Policies\ZipCodePolicy;
+
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -13,7 +47,21 @@ class AuthServiceProvider extends ServiceProvider
      * @var array<class-string, class-string>
      */
     protected $policies = [
-        //
+        Address::class => AddressPolicy::class,
+        Chat::class => ChatPolicy::class,
+        ChatRoom::class => ChatRoomPolicy::class,
+        City::class => CityPolicy::class,
+        Idea::class => IdeaPolicy::class,
+        Instrument::class => InstrumentPolicy::class,
+        Message::class => MessagePolicy::class,
+        Notification::class => NotificationPolicy::class,
+        Order::class => OrderPolicy::class,
+        PracticeDate::class => PracticeDatePolicy::class,
+        Product::class => ProductPolicy::class,
+        Role::class => RolePolicy::class,
+        Team::class => TeamPolicy::class,
+        User::class => UserPolicy::class,
+        ZipCode::class => ZipCodePolicy::class,
     ];
 
     /**
@@ -21,6 +69,7 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $this->registerPolicies();
+
     }
 }
