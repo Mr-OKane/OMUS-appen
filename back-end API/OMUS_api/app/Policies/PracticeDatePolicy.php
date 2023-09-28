9c88e2705847b54e83d6f6071bfccab09b89c68a
William Anker <William.s.anker@gmail.com>
2023-09-28 12:49:38 +0200

    Completed the PracticeDate Policy v1.14.7

--- back-end API/OMUS_api/app/Policies/PracticeDatePolicy.php
+++ back-end API/OMUS_api/app/Policies/PracticeDatePolicy.php
@@ -2,6 +2,7 @@
 
 namespace App\Policies;
 
+use App\Models\Permission;
 use App\Models\PracticeDate;
 use App\Models\User;
 use Illuminate\Auth\Access\Response;
@@ -11,47 +12,60 @@ class PracticeDatePolicy
     /**
      * Determine whether the user can view any models.
      */
-    public function viewAny(User $user): bool
+    public function viewAny(): Response
     {
-        //
+        $user = auth('sanctum')->user();
+        return $user->role->permissions->contains(Permission::withTrashed()->firstWhere('name','=','practiceDate.viewAny'))
+            ? Response::allow()
+            : Response::deny('You are not the chosen one',403);
     }
 
     /**
      * Determine whether the user can view the model.
      */
-    public function view(User $user, PracticeDate $practiceDate): bool
+    public function view(User $user): Response
     {
-        //
+        return $user->role->permissions->contains(Permission::withTrashed()->firstWhere('name','=','practiceDate.view'))
+            ? Response::allow()
+            : Response::deny('You are not the chosen one',403);
     }
 
     /**
      * Determine whether the user can create models.
      */
-    public function create(User $user): bool
+    public function create(User $user): Response
     {
-        //
+        return $user->role->permissions->contains(Permission::withTrashed()->firstWhere('name','=','practiceDate.view'))
+            ? Response::allow()
+            : Response::deny('You are not the chosen one',403);
     }
 
     /**
      * Determine whether the user can update the model.
      */
-    public function update(User $user, PracticeDate $practiceDate): bool
+    public function update(User $user): Response
     {
-        //
+        $authUser = auth('sanctum')->user();
+        return ($authUser['id'] === $user['id'] || $authUser->role->permissions->contains(Permission::withTrashed()
+                ->firstWhere('name','=','practiceDate.update')))
+            ? Response::allow()
+            : Response::deny('You are not the chosen one',403);
     }
 
     /**
      * Determine whether the user can delete the model.
      */
-    public function delete(User $user, PracticeDate $practiceDate): bool
+    public function delete(User $user): Response
     {
-        //
+        return $user->role->permissions->contains(Permission::withTrashed()->firstWhere('name','=','practiceDate.delete'))
+            ? Response::allow()
+            : Response::deny('You are not the chosen one',403);
     }
 
     /**
      * Determine whether the user can restore the model.
      */
-    public function restore(User $user, PracticeDate $practiceDate): bool
+    public function restore(User $user, PracticeDate $practiceDate)
     {
         //
     }
@@ -59,7 +73,7 @@ class PracticeDatePolicy
     /**
      * Determine whether the user can permanently delete the model.
      */
-    public function forceDelete(User $user, PracticeDate $practiceDate): bool
+    public function forceDelete(User $user, PracticeDate $practiceDate)
     {
         //
     }
