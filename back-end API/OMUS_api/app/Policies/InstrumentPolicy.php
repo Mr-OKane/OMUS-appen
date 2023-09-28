aebff393f42cbdc901f917346ebf8d38a578b1b7
William Anker <William.s.anker@gmail.com>
2023-09-28 12:44:59 +0200

    Completed the Instrument Policy v1.8.7

--- back-end API/OMUS_api/app/Policies/InstrumentPolicy.php
+++ back-end API/OMUS_api/app/Policies/InstrumentPolicy.php
@@ -3,6 +3,7 @@
 namespace App\Policies;
 
 use App\Models\Instrument;
+use App\Models\Permission;
 use App\Models\User;
 use Illuminate\Auth\Access\Response;
 
@@ -11,56 +12,84 @@ class InstrumentPolicy
     /**
      * Determine whether the user can view any models.
      */
-    public function viewAny(User $user): bool
+    public function viewAny(): Response
     {
-        //
+        $user = auth('sanctum')->user();
+        return $user->role->permissions->contains(Permission::withTrashed()->firstWhere('name','=','instrument.viewAny'))
+            ? Response::allow()
+            : Response::deny('You are not the chosen one',403);
+    }
+
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny_deleted(): Response
+    {
+        $user = auth('sanctum')->user();
+        return $user->role->permissions->contains(Permission::withTrashed()->firstWhere('name','=','instrument.viewAny.deleted'))
+            ? Response::allow()
+            : Response::deny('You are not the chosen one',403);
     }
 
     /**
      * Determine whether the user can view the model.
      */
-    public function view(User $user, Instrument $instrument): bool
+    public function view(User $user): Response
     {
-        //
+        return $user->role->permissions->contains(Permission::withTrashed()->firstWhere('name','=','instrument.view'))
+            ? Response::allow()
+            : Response::deny('You are not the chosen one',403);
     }
 
     /**
      * Determine whether the user can create models.
      */
-    public function create(User $user): bool
+    public function create(User $user): Response
     {
-        //
+        return $user->role->permissions->contains(Permission::withTrashed()->firstWhere('name','=','instrument.create'))
+            ? Response::allow()
+            : Response::deny('You are not the chosen one',403);
     }
 
     /**
      * Determine whether the user can update the model.
      */
-    public function update(User $user, Instrument $instrument): bool
+    public function update(User $user): Response
     {
-        //
+        $authUser = auth('sanctum')->user();
+        return ($authUser['id'] === $user['id'] || $authUser->role->permissions->contains(Permission::withTrashed()
+                ->firstWhere('name','=','instrument.update')))
+            ? Response::allow()
+            : Response::deny('You are not the chosen one',403);
     }
 
     /**
      * Determine whether the user can delete the model.
      */
-    public function delete(User $user, Instrument $instrument): bool
+    public function delete(User $user): Response
     {
-        //
+        return $user->role->permissions->contains(Permission::withTrashed()->firstWhere('name','=','instrument.delete'))
+            ? Response::allow()
+            : Response::deny('You are not the chosen one',403);
     }
 
     /**
      * Determine whether the user can restore the model.
      */
-    public function restore(User $user, Instrument $instrument): bool
+    public function restore(User $user): Response
     {
-        //
+        return $user->role->permissions->contains(Permission::withTrashed()->firstWhere('name','=','instrument.restore'))
+            ? Response::allow()
+            : Response::deny('You are not the chosen one',403);
     }
 
     /**
      * Determine whether the user can permanently delete the model.
      */
-    public function forceDelete(User $user, Instrument $instrument): bool
+    public function forceDelete(User $user): Response
     {
-        //
+        return $user->role->permissions->contains(Permission::withTrashed()->firstWhere('name','=','instrument.force.delete'))
+            ? Response::allow()
+            : Response::deny('You are not the chosen one',403);
     }
 }
