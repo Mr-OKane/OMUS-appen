98e733f1d9943b9fd0d213a8c33f9301d1a4f1a2
William Anker <William.s.anker@gmail.com>
2023-09-28 12:54:19 +0200

    Completed the ZipCode Policy v1.21.7

--- back-end API/OMUS_api/app/Policies/ZipCodePolicy.php
+++ back-end API/OMUS_api/app/Policies/ZipCodePolicy.php
@@ -2,6 +2,7 @@
 
 namespace App\Policies;
 
+use App\Models\Permission;
 use App\Models\User;
 use App\Models\ZipCode;
 use Illuminate\Auth\Access\Response;
@@ -11,23 +12,28 @@ class ZipCodePolicy
     /**
      * Determine whether the user can view any models.
      */
-    public function viewAny(User $user): bool
+    public function viewAny(): Response
     {
-        //
+        $user = auth('sanctum')->user();
+        return $user->role->permissions->contains(Permission::withTrashed()->firstWhere('name','=','zipcode.viewAny'))
+            ? Response::allow()
+            : Response::deny('You are not the chosen one',403);
     }
 
     /**
      * Determine whether the user can view the model.
      */
-    public function view(User $user, ZipCode $zipCode): bool
+    public function view(User $user): Response
     {
-        //
+        return $user->role->permissions->contains(Permission::withTrashed()->firstWhere('name','=','zipcode.view'))
+            ? Response::allow()
+            : Response::deny('You are not the chosen one',403);
     }
 
     /**
      * Determine whether the user can create models.
      */
-    public function create(User $user): bool
+    public function create(User $user)
     {
         //
     }
@@ -35,7 +41,7 @@ class ZipCodePolicy
     /**
      * Determine whether the user can update the model.
      */
-    public function update(User $user, ZipCode $zipCode): bool
+    public function update(User $user, ZipCode $zipCode)
     {
         //
     }
@@ -43,7 +49,7 @@ class ZipCodePolicy
     /**
      * Determine whether the user can delete the model.
      */
-    public function delete(User $user, ZipCode $zipCode): bool
+    public function delete(User $user, ZipCode $zipCode)
     {
         //
     }
@@ -51,7 +57,7 @@ class ZipCodePolicy
     /**
      * Determine whether the user can restore the model.
      */
-    public function restore(User $user, ZipCode $zipCode): bool
+    public function restore(User $user, ZipCode $zipCode)
     {
         //
     }
@@ -59,7 +65,7 @@ class ZipCodePolicy
     /**
      * Determine whether the user can permanently delete the model.
      */
-    public function forceDelete(User $user, ZipCode $zipCode): bool
+    public function forceDelete(User $user, ZipCode $zipCode)
     {
         //
     }
