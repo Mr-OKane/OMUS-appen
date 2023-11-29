89fdcfd5a079d2725a1d1be277bb43c9455bd132
William Anker <William.s.anker@gmail.com>
2023-11-29 14:42:14 +0100

    removed deleted_at column from messages migration v1.9.1.2

--- back-end API/OMUS_api/database/migrations/2023_08_07_120900_create_messages_table.php
+++ back-end API/OMUS_api/database/migrations/2023_08_07_120900_create_messages_table.php
@@ -17,7 +17,6 @@ return new class extends Migration
             $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
             $table->string('message');
             $table->timestamps();
-            $table->timestamp('deleted_at')->nullable();
         });
     }
 
