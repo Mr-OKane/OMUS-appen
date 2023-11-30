36b737658a3e99a59d26cd67565f38ffcfb3b076
William Anker <William.s.anker@gmail.com>
2023-11-30 10:44:24 +0100

    Modified the index and deleted return data and modified Store to take address better in Order Controller v1.11.6.4

--- back-end API/OMUS_api/app/Http/Controllers/OrderController.php
+++ back-end API/OMUS_api/app/Http/Controllers/OrderController.php
@@ -2,10 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Address;
+use App\Models\City;
 use App\Models\Order;
 use App\Http\Requests\StoreOrderRequest;
 use App\Http\Requests\UpdateOrderRequest;
 use App\Models\User;
+use App\Models\ZipCode;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 
@@ -24,8 +27,7 @@ class OrderController extends Controller
             return response()->json(['message' => "1000+ pagination per Page is to much"],400);
         }
 
-        $orders = Order::with('user.address.zipCode.city')
-            ->with('address.zipCode.city')->with('products')
+        $orders = Order::with('user')->with('address.zipCode.city')
             ->paginate($paginationPerPage);
 
         return response()->json(['object' => $orders]);
@@ -41,7 +43,7 @@ class OrderController extends Controller
             return response()->json(['message' => "1000+ orders per page is to much"],400);
         }
 
-        $orders = Order::onlyTrashed()->with('user.address.zipCode.city')
+        $orders = Order::onlyTrashed()->with('user')
             ->with('address.zipCode.city')->paginate($paginationPerPage);
 
         return response()->json(['object' => $orders]);
@@ -55,9 +57,43 @@ class OrderController extends Controller
         $this->authorize('create', [Order::class,$user]);
 
         $request->validated();
+        $address = Address::withoutTrashed()->firstWhere('address','=' ,$request["address"]);
+        $zipCode = ZipCode::withoutTrashed()->firstWhere('zipCode','=', $request['zipCode']);
+        $city = City::withoutTrashed()->firstWhere('city','=', $request['city']);
 
         $order = new Order();
-        $order->address()->associate($request['address']);
+        if (empty($address))
+        {
+            $newAddress = new Address();
+            $newAddress['address'] = $request['address'];
+            if (empty($zipCode))
+            {
+                $newZipCode = new ZipCode();
+                $newZipCode['zip_code'] = $request['zipCode'];
+                if (empty($city))
+                {
+                    $newCity = new City();
+                    $newCity['city'] = $request['city'];
+                    $newCity->save();
+
+                    $newZipCode->city()->associate($newCity['id']);
+                }
+                else
+                {
+                    $newZipCode->city()->associate($city['id']);
+                }
+
+                $newZipCode->save();
+
+                $newAddress->zipCode()->associate($newZipCode['id']);
+            }else
+            {
+                $newAddress->zipCode()->associate($zipCode['id']);
+            }
+        }else
+        {
+            $order->address()->associate($address);
+        }
         $order['status'] = $request['status'];
         $order->user()->associate($request['user']);
         $order['order_date'] = Carbon::now();
@@ -69,7 +105,6 @@ class OrderController extends Controller
         $object->address->zipCode->city;
         $object->products;
         $object->user->role;
-        $object->user->address->zipCode->city;
 
         return response()->json(['message' => "created the order successfully",'object' => $object],201);
     }
@@ -106,7 +141,6 @@ class OrderController extends Controller
         {
             $object['status'] = $request['status'];
         }
-        $object->address()->associate($request['address']);
 
         $object->save();
 
