208ec35ce4a05c81d4abe39112639447fc3b1cc1
William Anker <William.s.anker@gmail.com>
2024-02-08 13:44:03 +0100

    Node_modules update on node outside the projekt

--- OMUS-frontend/OMUS-webapp/pages/index.vue
+++ OMUS-frontend/OMUS-webapp/pages/index.vue
@@ -5,6 +5,7 @@ const route = useRoute()
 <template>
   <div>
     <h1>Nuxt Routing set up successfully!</h1>
+    <p>Welcome to the home page</p>
     <p>Current route: {{ route.path }}</p>
     <a href="https://nuxt.com/docs/getting-started/routing" target="_blank">Learn more about Nuxt Routing</a>
   </div>
